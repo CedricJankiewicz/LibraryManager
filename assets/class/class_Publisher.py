b5a8c1e1cd37b0492a2ba54981446764066fcd85
py50eum <py50eum@eduvaud.ch>
2025-12-03 13:56:42 +0100

    Création du fichier class_Publisher +exemple en commentaire

---
+++ assets/class/class_Publisher.py
@@ -0,0 +1,29 @@
+"""
+Auteur : Loïc
+Date de création : 03.12.2025
+Description du fichier : class publisher
+V 1.0
+
+Derniere modif date: 03.12.2025
+Dernière modif description :
+
+"""
+
+class Publisher:
+    def __init__(self, name):
+        self.name = name
+
+
+
+
+
+"""
+Exemple de création d'objets
+
+
+publisher_1 = Publisher("Lucky Comics")
+publisher_2 = Publisher("Dargo Suisse")
+
+print(publisher_1.name)
+print(publisher_2.name)
+"""
