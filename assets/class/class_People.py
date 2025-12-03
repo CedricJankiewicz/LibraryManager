8e136fca13119c06421c6eb97fea4530b0b34252
py50eum <py50eum@eduvaud.ch>
2025-12-03 14:01:50 +0100

    Création du fichier class_People + exemple en commentaire

---
+++ assets/class/class_People.py
@@ -0,0 +1,32 @@
+"""
+Auteur : Loïc
+Date de création : 03.12.2025
+Description du fichier : class People ;il sera heriter par la suite
+V 1.0
+
+Derniere modif date: 03.12.2025
+Dernière modif description :
+
+"""
+
+class Person:
+    def __init__(self, name, firstname):
+        self.name = name
+        self.firstname = firstname
+
+
+"""
+exemple de création d'objets
+
+
+Jack = Person("Jack", "John")
+
+
+print(Jack.name, Jack.firstname)
+
+"""
+
+
+
+
+
