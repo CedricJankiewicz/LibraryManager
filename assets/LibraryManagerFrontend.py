5e3e18f2e3b5850876778269ccfe89b94b1fa1ce
CedricJankiewicz <pm14cau@eduvaud.ch>
2025-12-03 15:35:40 +0100

    Fichier frontend et 1'er pages de l'appli

---
+++ assets/LibraryManagerFrontend.py
@@ -0,0 +1,120 @@
+"""
+Program name : LibraryManagerFrontend.py
+Date : 03.12.2025
+Edit : 03.12.2025
+Description : -
+"""
+#imports
+from customtkinter import *
+
+##############################
+#####      Variable      #####
+##############################
+
+##############################
+#####      Function      #####
+##############################
+
+def header_selection(button):
+
+    for i, btn in enumerate(btn_navbar.values()):
+        btn.configure(**HEADER_DEFAULT_STYLE)
+
+    btn_navbar[button].configure(**HEADER_ACTIVE_STYLE)
+
+
+##############################
+#####        Style       #####
+##############################
+
+#font
+default_font = ("TkDefaultFont", 25, "bold")
+widget_font = ("TkDefaultFont", 30, "bold")
+
+#widget style
+HEADER_DEFAULT_STYLE = {
+    "fg_color": ["gray92", "gray14"],
+    "text_color": ["gray14", "gray84"],
+    "corner_radius": 0,
+    "border_width": 1,
+    "height": 80,
+    "font": widget_font
+}
+
+HEADER_ACTIVE_STYLE = {
+    "fg_color": ["gray92", "gray14"],
+    "text_color": ["gray14", "gray84"],
+    "corner_radius": 0,
+    "border_width": 0,
+    "height": 80,
+    "font": widget_font
+}
+
+DROP_LIST_STYLE = {
+    "fg_color": ["gray86", "gray20"],
+    "button_color": ["gray82", "gray24"],
+    "text_color": ["gray14", "gray84"]
+}
+
+SEARCH_RESULT_STYLE = {
+    "fg_color": ["gray80", "gray24"],
+    "height": 50
+}
+
+##############################
+#####     Interface      #####
+##############################
+
+#Window config
+window = CTk()
+window.geometry("1440x960")
+
+#header
+frm_header = CTkFrame(window, fg_color="transparent")
+frm_header.pack(side="top", fill="x")
+
+for i in range(6):
+    frm_header.grid_columnconfigure(i, weight=1)
+
+btn_navbar = {}
+
+btn_navbar["search"] = CTkButton(frm_header, text="Rechercher\nun livre", **HEADER_ACTIVE_STYLE, command=lambda : header_selection("search"))
+btn_navbar["borrow"] = CTkButton(frm_header, text="Emprunter\nun livre", **HEADER_DEFAULT_STYLE, command=lambda : header_selection("borrow"))
+btn_navbar["return"] = CTkButton(frm_header, text="Rendre\nun livre", **HEADER_DEFAULT_STYLE,command=lambda : header_selection("return"))
+btn_navbar["client"] = CTkButton(frm_header, text="Client", **HEADER_DEFAULT_STYLE,command=lambda : header_selection("client"))
+btn_navbar["manage"] = CTkButton(frm_header, text="Gestion\nde livre", **HEADER_DEFAULT_STYLE,command=lambda : header_selection("manage"))
+
+for i, btn in enumerate(btn_navbar.values()):
+    btn.grid(column=i, row=0, sticky="ew")
+
+btn_account = CTkButton(frm_header, text="Se Connecter", **HEADER_DEFAULT_STYLE).grid(column=5, row=0, sticky="ew")
+
+#search window
+frm_search = CTkFrame(window, fg_color="transparent")
+frm_search.pack(expand=True, fill="both", pady=20, padx=20)
+
+frm_search_searching = CTkFrame(frm_search, fg_color="transparent")
+frm_search_searching.pack(fill="x", pady=(0, 20), padx=150)
+
+ent_search_searchbar = CTkEntry(frm_search_searching, placeholder_text="Rechercher...", width=400, font=widget_font)
+ent_search_searchbar.pack(side="left")
+
+search_options = ["Titre", "Auteur", "Id"]
+
+drp_search_search_by = CTkOptionMenu(frm_search_searching, font=widget_font, values=search_options, **DROP_LIST_STYLE)
+drp_search_search_by.set("Titre")
+drp_search_search_by.pack(side="right")
+
+lbl_search_search_by = CTkLabel(frm_search_searching, text="rechercher par : ", font=widget_font)
+lbl_search_search_by.pack(side="right")
+
+frm_search_results = CTkScrollableFrame(frm_search)
+frm_search_results.pack(expand=True, fill="both")
+
+frm_search_result = CTkFrame(frm_search_results, **SEARCH_RESULT_STYLE)
+frm_search_result.pack(fill="x", pady=20, padx=20)
+
+lbl_search_result = CTkLabel(frm_search_result, text="titre : genre : auteur : maison d\'édition : date de parrution : Disponible", font=default_font)
+lbl_search_result.pack(pady=20, padx=20)
+
+window.mainloop()
