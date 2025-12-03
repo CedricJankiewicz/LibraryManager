175f19f555a76686e95fa8435b38bfdc412853fb
py50eum <py50eum@eduvaud.ch>
2025-12-03 14:42:57 +0100

    Ajout de l'id dans Worker

--- assets/class/class_Worker.py
+++ assets/class/class_Worker.py
@@ -2,27 +2,27 @@
 Auteur : Loïc
 Date de création : 03.12.2025
 Description du fichier : class Worker hérite nom prénom de people
-V 1.0
+V 1.1
 
 Derniere modif date: 03.12.2025
-Dernière modif description :
+Dernière modif description : Ajout de l'id
 
 """
 from class_People import *
 
 
 class Worker(Person):
-    def __init__(self,firstname,lastname,Schedule,e_mail):
-        super().__init__(firstname,lastname)
+    def __init__(self,id,firstname,lastname,Schedule,e_mail):
+        super().__init__(id,firstname,lastname)
         self.Schedule = Schedule
         self.e_mail = e_mail
 
 """   
 Exemple d'objets
 
-Jack = Worker("jack","Johnes","09-25-45","jack.johnes@eduvaud.ch")
+Jack = Worker("1",jack","Johnes","09-25-45","jack.johnes@eduvaud.ch")
 
-print(Jack.firstname,Jack.lastname,Jack.Schedule,Jack.e_mail)
+print(Jack.id,Jack.firstname,Jack.lastname,Jack.Schedule,Jack.e_mail)
 
 """
 
